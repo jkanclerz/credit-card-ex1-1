33f3d6d800fffa606c50ee66838bfdb27b18293e
Rafal Kielbowicz <kiel.rafal@gmail.com>
2018-10-11 09:04:34 +0000

    added creditcard class

--- creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
+++ creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
@@ -5,4 +5,15 @@ import org.junit.Test;
 
 public class CreditCardTest {
     
+    @Test
+    public void assignCreditLimitToCard() {
+        
+        CreditCard card = new CreditCard();
+        card.assignLimit(2000);
+        
+        Assert.assertTrue(card.getLimit() == 2000);
+    }
+    
+
+    
 }
