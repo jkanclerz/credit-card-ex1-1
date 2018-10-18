85d966b414d65cf74ae8a99a7f9f744bdf06e411
Rafal Kielbowicz <kiel.rafal@gmail.com>
2018-10-18 08:30:28 +0000

    Revert "added comment"
    This reverts commit 43eccd780b386a2f5e079d3a5a0c2c155930caad.

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -4,7 +4,7 @@ class CreditCard {
     public void assignLimit(double limit) {
         
     }
-    // get limit return current card limit
+    
     public double getLimit() {
         return 2000;
     }
