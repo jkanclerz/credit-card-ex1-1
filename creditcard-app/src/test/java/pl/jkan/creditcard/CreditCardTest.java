93d288fab60907050b5f1be6f2bf98b714c5147f
RafalKielbowicz <kiel.rafal@gmail.com>
2018-10-18 09:07:33 +0000

    .gitignore fix

--- creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
+++ creditcard-app/src/test/java/pl/jkan/creditcard/CreditCardTest.java
@@ -14,6 +14,20 @@ public class CreditCardTest {
         Assert.assertTrue(card.getLimit() == 2000);
     }
     
-
+     @Test
+    public void cantWithdrowWhenNotEnoughFounds() {
+        
+    }
+    
+    @Test
+    public void repayCredit() {
+        
+        CreditCard card = new CreditCard();
+        card.assignLimit(2000);
+        
+        card.withdraw(1000);
+        
+        Assert.assertTrue(card.Saldo() == 1000);
+    }
     
 }
