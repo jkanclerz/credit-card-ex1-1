1fa859a321704f46d242e17251dab6d08d4e988b
EC2 Default User <ec2-user@ip-172-31-0-141.eu-west-1.compute.internal>
2018-10-25 08:22:58 +0000

    added some features

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -1,11 +1,22 @@
 package pl.jkan.creditcard;
 
 class CreditCard {
+    
+    private boolean blockade = false;
+    
     public void assignLimit(double limit) {
         
     }
     
     public double getLimit() {
         return 2000;
+    }
+    
+    public void block() {
+        this.blockade = true;
+    }
+    
+    public boolean isBlocked() {
+        return this.blockade;
     }
 }
